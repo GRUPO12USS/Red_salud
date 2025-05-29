a25e522b39928d3bbc9a31db751ef8f1457c78a2
PabloRiveros <priverosm1@correo.uss.cl>
2025-05-28 23:18:03 -0400

    Subida de archivos de proyecto con Frontend, Backend y DB

---
+++ backend/reservas/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Cita
+from .serializers import CitaSerializer
+
+class CitaViewSet(viewsets.ModelViewSet):
+    queryset = Cita.objects.all()
+    serializer_class = CitaSerializer
+
